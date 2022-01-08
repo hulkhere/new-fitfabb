fd26881873b543dfb503b3cc71702ae52c50fa33
Hulkhere <vishnubhupathiamudha@gmail.com>
2022-01-08 17:47:41 +0530

    ctas-format

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import Header from "../src/header"
 import ImageSlider from "../src/components/ImageSlider";
 import { SliderData } from "../src/components/SliderData";
-
+import Ctas from "../src/ctas";
 function App() {
   return (
     <>
       <Header />
       <ImageSlider slides={SliderData} />;
+      <Ctas />
     </>
   );
 }
---
+++ src/ctas.jsx
@@ -0,0 +1,41 @@
+export default function Ctas(){
+
+    return (
+      <>
+        <div className="w-full h-max flex justify-center items-center flex-col gap-5 md:flex-row md:p-5">
+          <div className="bg-green-300 w-64 h-56 rounded-xl flex flex-col items-center">
+            <h1 className="text-4xl">SILVER</h1>
+            <div>
+              <li>aksfkjasfaskjdhkjashd</li>
+              <li>bfhjfhakfaskjsghagsfh</li>
+              <li>aksfkjasfaskjdhkjashd</li>
+              <li>aksfkjasfaskjdhkjashd</li>
+            </div>
+            <button className="bg-blue-400 px-4 mt-6">JOIN NOW @499</button>
+          </div>
+
+          <div className="bg-green-300 w-64 h-56 rounded-xl flex flex-col items-center">
+            <h1 className="text-4xl">GOLD</h1>
+            <div>
+              <li>aksfkjasfaskjdhkjashd</li>
+              <li>bfhjfhakfaskjsghagsfh</li>
+              <li>aksfkjasfaskjdhkjashd</li>
+              <li>aksfkjasfaskjdhkjashd</li>
+            </div>
+            <button className="bg-blue-400 px-4 mt-6">JOIN NOW @699</button>
+          </div>
+
+          <div className="bg-green-300 w-64 h-56 rounded-xl flex flex-col items-center">
+            <h1 className="text-4xl">PLATINUM</h1>
+            <div>
+              <li>aksfkjasfaskjdhkjashd</li>
+              <li>bfhjfhakfaskjsghagsfh</li>
+              <li>aksfkjasfaskjdhkjashd</li>
+              <li>aksfkjasfaskjdhkjashd</li>
+            </div>
+            <button className="bg-blue-400 px-4 mt-6">JOIN NOW @2499</button>
+          </div>
+        </div>
+      </>
+    );
+}
