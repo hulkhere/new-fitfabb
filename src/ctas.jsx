02789fcafcaca0072b3708315685a36dfc3121b1
Hulkhere <vishnubhupathiamudha@gmail.com>
2022-01-10 21:55:22 +0530

    colors

--- src/ctas.jsx
+++ src/ctas.jsx
@@ -4,7 +4,9 @@ export default function Ctas(){
       <>
         <div className="w-full h-max flex justify-center items-center flex-col gap-5 md:flex-row md:p-5 xl:p-0 gap-10">
           <div className="bg-green-300 w-64 h-56 rounded-xl flex flex-col items-center">
-            <h1 className="text-4xl">SILVER</h1>
+            <h1 className="text-4xl" style={{ color: "#C0C0C0" }}>
+              SILVER
+            </h1>
             <div>
               <li>aksfkjasfaskjdhkjashd</li>
               <li>bfhjfhakfaskjsghagsfh</li>
@@ -15,7 +17,9 @@ export default function Ctas(){
           </div>
 
           <div className="bg-green-300 w-64 h-56 rounded-xl flex flex-col items-center">
-            <h1 className="text-4xl">GOLD</h1>
+            <h1 className="text-4xl" style={{ color: "#FFD700" }}>
+              GOLD
+            </h1>
             <div>
               <li>aksfkjasfaskjdhkjashd</li>
               <li>bfhjfhakfaskjsghagsfh</li>
@@ -26,7 +30,9 @@ export default function Ctas(){
           </div>
 
           <div className="bg-green-300 w-64 h-56 rounded-xl flex flex-col items-center">
-            <h1 className="text-4xl">PLATINUM</h1>
+            <h1 className="text-4xl" style={{ color: "#E5E4E2" }}>
+              PLATINUM
+            </h1>
             <div>
               <li>aksfkjasfaskjdhkjashd</li>
               <li>bfhjfhakfaskjsghagsfh</li>
