eee32ceab7a20fd0a9349ff3b616c8b9175ee128
Hulkhere <vishnubhupathiamudha@gmail.com>
2022-01-11 20:41:14 +0530

    logoo

--- src/ctas.jsx
+++ src/ctas.jsx
@@ -3,11 +3,11 @@ export default function Ctas(){
     return (
       <>
         <div className="w-full h-max flex justify-center items-center flex-col gap-5 md:flex-row md:p-5 xl:p-0 gap-10">
-          <div className="bg-green-300 w-64 h-56 rounded-xl flex flex-col items-center">
+          <div className="bg-black w-64 h-56 rounded-xl flex flex-col items-center">
             <h1 className="text-4xl" style={{ color: "#C0C0C0" }}>
               SILVER
             </h1>
-            <div>
+            <div className="text-white">
               <li>aksfkjasfaskjdhkjashd</li>
               <li>bfhjfhakfaskjsghagsfh</li>
               <li>aksfkjasfaskjdhkjashd</li>
@@ -20,7 +20,7 @@ export default function Ctas(){
             <h1 className="text-4xl" style={{ color: "#FFD700" }}>
               GOLD
             </h1>
-            <div>
+            <div className="text-white">
               <li>aksfkjasfaskjdhkjashd</li>
               <li>bfhjfhakfaskjsghagsfh</li>
               <li>aksfkjasfaskjdhkjashd</li>
@@ -33,7 +33,7 @@ export default function Ctas(){
             <h1 className="text-4xl" style={{ color: "#E5E4E2" }}>
               PLATINUM
             </h1>
-            <div>
+            <div className="text-white">
               <li>aksfkjasfaskjdhkjashd</li>
               <li>bfhjfhakfaskjsghagsfh</li>
               <li>aksfkjasfaskjdhkjashd</li>
