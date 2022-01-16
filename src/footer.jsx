2843c7999948ee5b15334219e75c27b80174c742
Hulkhere <vishnubhupathiamudha@gmail.com>
2022-01-16 16:19:31 +0530

    54654

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,6 +4,7 @@ import Call from "../src/icons/telephone.png"
 import Facebook from "../src/icons/facebook.png";
 import Gmail from "../src/icons/gmail.png";
 import Youtube from "../src/icons/youtube.png";
+import ImgFooter from "../src/images/gym-footer.png"
 
 
 
@@ -12,24 +13,39 @@ export default function Footer(){
     return (
       <>
         <footer
+          className="bg-cover text-green-300"
           style={{
-            backgroundImage: `url("${Instagram}")`,
+            backgroundImage: `url("${ImgFooter}")`,
           }}
         >
-          <div className="flex justify-between items-center md:items-center gap-5 mx-10 lg:mx-40">
+          <div className="flex justify-between items-center gap-5 mx-10 lg:mx-40">
             <div className="text-2xl md:text-5xl">
               <h1>
                 Your body can stand almost anything. It’s your mind that you
                 have to convince.
               </h1>
             </div>
+
+          
             <div className="h-auto w-20 flex gap-5 flex-col">
-              <img src={Instagram} alt="" />
-              <img src={Call} alt="" />
-              <img src={Facebook} alt="" />
-              <img src={Whatsapp} alt="" />
-              <img src={Gmail} alt="" />
-              <img src={Youtube} alt="" />
+              <a href="">
+                <img src={Instagram} alt="" />
+              </a>
+              <a href="">
+                <img src={Whatsapp} alt="" />
+              </a>
+              <a href="">
+                <img src={Call} alt="" />
+              </a>
+              <a href="">
+                <img src={Facebook} alt="" />
+              </a>
+              <a href="">
+                <img src={Gmail} alt="" />
+              </a>
+              <a href="">
+                <img src={Youtube} alt="" />
+              </a>
             </div>
           </div>
 
